4f25ef436947bb4235879686c536eed879dc7ced
Ajay2c <ajaychandru20@gmail.com>
2023-04-14 23:06:43 +0530

    added column attributes

--- Unions/(UNION) Excercise 22.sql
+++ Unions/(UNION) Excercise 22.sql
@@ -1,15 +1,23 @@
-SELECT customer_id, first_name, points, 'Bronze' AS Medals
-FROM customers
-WHERE (points < 2000)
+USE sql_store;
+SELECT 	customer_id,
+	first_name,
+        points,
+        'Bronze' AS type
+FROM 	customers
+WHERE	points < 2000
 UNION
-SELECT customer_id, first_name, points, 'Silver' AS Medals
-FROM customers 
-WHERE points BETWEEN 2000 AND 3000
+SELECT 	customer_id,
+	first_name,
+        points,
+        'Silver' AS type
+FROM 	customers
+WHERE 	points BETWEEN 2000 AND 3000
 UNION
-SELECT customer_id, first_name, points, 'Gold' AS Medals
-FROM customers 
-WHERE (points >= 3000)
-
-ORDER BY points DESC
-
+SELECT 	customer_id,
+	first_name,
+        points,
+        'Gold' AS type
+FROM 	customers
+WHERE 	points >=3000
+ORDER BY first_name
 
